a6e0d4d7c1b84130eb5d25bbdb314c30b1c08644
Prince-1562 <princemakani1562@gmail.com>
2025-05-13 07:46:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+    // Remove leading whitespace
+    str = str.trimStart();
+    
+    // Find the index of the first space
+    const spaceIndex = str.indexOf(' ');
+    
+    // If no space is found, return the entire string
+    if (spaceIndex === -1) {
+        return str;
+    }
+    
+    // Return the substring up to the first space
+    return str.substring(0, spaceIndex);
 }
 
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
